408c76ed5a203c72248a5f6f527396eb660c9ac6
Jixun Wu <Jixun.Moe@Gmail.Com>
2017-01-13 20:20:46 +0000

    use space instead of tab

--- src/adclean.js
+++ src/adclean.js
@@ -1,61 +1,61 @@
 ;(function (document, window, hostname, blankFunction) {
-    var each = Array.prototype.forEach;
+  var each = Array.prototype.forEach;
 
-	// 根据当前访问域名判定要使用的代码
-	switch (hostname) {
-		case 'm.lwxs.com':
-			removeTanx();
-			removeMethods(['qijixs_top', 'qijixs_middle', 'qijixs_bottom']);
-			break;
+  // 根据当前访问域名判定要使用的代码
+  switch (hostname) {
+    case 'm.lwxs.com':
+      removeTanx();
+      removeMethods(['qijixs_top', 'qijixs_middle', 'qijixs_bottom']);
+      break;
 
-		case 'm.33yq.com':
-			removeTanx();
-			removeMethods(['nryhf', 'qijixs_middle', 'qijixs_bottom', 'qijixs_dingbuxuanfu']);
-			break;
+    case 'm.33yq.com':
+      removeTanx();
+      removeMethods(['nryhf', 'qijixs_middle', 'qijixs_bottom', 'qijixs_dingbuxuanfu']);
+      break;
 
-		case 'm.69shu.com':
-			removeGoogleAds();
-			break;
-	}
+    case 'm.69shu.com':
+      removeGoogleAds();
+      break;
+  }
 
-	// 移除 tanx.com 广告的展位块
-	function removeTanx () {
-		document.body.style.marginTop = 0;
-        var tanx = document.querySelectorAll('div>a[id^="tanx"]');
-        each.call(tanx, function (t) {
-            var parentStyle = t.parentNode.style;
-            if (parentStyle.cssText.indexOf('margin') != -1)
-                parentStyle.display = "none";
+  // 移除 tanx.com 广告的展位块
+  function removeTanx () {
+    document.body.style.marginTop = 0;
+    var tanx = document.querySelectorAll('div>a[id^="tanx"]');
+    each.call(tanx, function (t) {
+      var parentStyle = t.parentNode.style;
+      if (parentStyle.cssText.indexOf('margin') != -1)
+        parentStyle.display = "none";
+    });
+
+    window.qijixs_bottom = function () {};
+  }
+
+  // 移除函数，或禁止建立
+  function removeMethods (methods, obj) {
+    obj = obj || window;
+
+    each.call(methods, function (method) {
+      if (method in obj) {
+        obj[method] = blankFunction;
+      } else {
+        Object.defineProperty(obj, method, {
+          value: blankFunction
         });
+      }
+    });
+  }
 
-		window.qijixs_bottom = function () {};
-	}
+  // 移除谷歌广告的空格
+  function removeGoogleAds () {
+    var s = document.createElement('style');
+    s.textContent = '.adsbygoogle{display:none !important}';
+    document.head.appendChild(s);
 
-	// 移除函数，或禁止建立
-	function removeMethods (methods, obj) {
-		obj = obj || window;
-
-		each.call(methods, function (method) {
-			if (method in obj) {
-				obj[method] = blankFunction;
-			} else {
-				Object.defineProperty(obj, method, {
-					value: blankFunction
-				});
-			}
-		});
-	}
-
-	// 移除谷歌广告的空格
-	function removeGoogleAds () {
-		var s = document.createElement('style');
-		s.textContent = '.adsbygoogle{display:none !important}';
-		document.head.appendChild(s);
-
-		each.call(document.getElementsByClassName('adsbygoogle'), function (b) {
-			b.parentNode.removeChild(b);
-		});
-	}
+    each.call(document.getElementsByClassName('adsbygoogle'), function (b) {
+      b.parentNode.removeChild(b);
+    });
+  }
 })(document, window, location.hostname,
-	function /* blank function */() {}
+  function /* blank function */() {}
 );
--- src/loader.js
+++ src/loader.js
@@ -3,13 +3,13 @@
 // server will redirect triffic to this file.
 
 (function (window, document) {
-	// 15 days in ms: 1296000000
-	// Force a new version every 15 days
-	if (!window.__AD_CLEAR__) {
-		window.__AD_CLEAR__ = 1;
+  // 15 days in ms: 1296000000
+  // Force a new version every 15 days
+  if (!window.__AD_CLEAR__) {
+    window.__AD_CLEAR__ = 1;
 
-		var script = document.createElement('script');
-		script.src = 'https://hosts.jixun.moe/adclean/adclean.js?v' + (~~((+new Date()) / 1296000000));
-        document.head.appendChild(script);
-	}
+    var script = document.createElement('script');
+    script.src = 'https://hosts.jixun.moe/adclean/adclean.js?v' + (~~((+new Date()) / 1296000000));
+    document.head.appendChild(script);
+  }
 })(window, document);
